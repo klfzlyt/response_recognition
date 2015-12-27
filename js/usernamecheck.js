71b43a1642bdbe4000fe073b2ffe9610a4521a37
klfzlyt <327442095@qq.com>
2015-12-27 17:57:20 +0800

    Signed-off-by: klfzlyt <327442095@qq.com>

---
+++ js/usernamecheck.js
@@ -0,0 +1,28 @@
+function showHint(str)
+{
+var xmlhttp;
+if (str.length==0)
+  { 
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+//  	window.alert(xmlhttp.readyState);
+// 	window.alert(xmlhttp.status);
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","js/ajaxcheckname.php?q="+str,true);
+xmlhttp.send();
+}
